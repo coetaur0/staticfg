1e17516e03d3d1ec2b0a8e9c67afc5fda4f52979
Aurelien Coet <aurelien.coet19@gmail.com>
2018-05-08 11:20:53 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 
 setup(name='staticfg',
       version=0.1,
-      description='Control flow graph generator for Python3 programs',
+      url='https://github.com/coetaur0/staticfg',
+      license='Apache 2',
       author='Aurelien Coet',
       author_email='aurelien.coet19@gmail.com',
+      description='Control flow graph generator for Python3 programs',
       packages=['staticfg'],
       install_requires=[
         'astor',
